0ee348ab8475aeb529822263ca7ad459c632c832
adambonneruk <adam@bonner.uk>
2020-09-05 17:23:24 +0100

    generating v4 uuids to the count specified

--- unique_generate.py
+++ unique_generate.py
@@ -2,14 +2,15 @@ import uuid
 import sys
 
 def print_uuid(version = 4):
-	print(uuid.uuid4())
+	if version == 1:
+		print("v1")
+	elif version == 2:
+		print("v2")
+	elif version == 3:
+		print("v3")
+	elif version == 4:
+		print(uuid.uuid4())
 
 def generate(version = 4, count = 1):
-	#print("")
-	#print("function: generatep")
-	#print("function version:" + str(version))
-	#print("function count:" + str(count))
-	#print(uuid.uuid4())s
-
 	for i in range(1, count + 1):
-		print_uuid()
+		print_uuid(version)
