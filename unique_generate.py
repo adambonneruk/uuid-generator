01062f9827300b134092a82204fa51d0f0607a77
adambonneruk <adam@bonner.uk>
2020-09-05 17:18:54 +0100

    generate a number of uuids based on a count

--- unique_generate.py
+++ unique_generate.py
@@ -1,9 +1,15 @@
 import uuid
 import sys
 
+def print_uuid(version = 4):
+	print(uuid.uuid4())
+
 def generate(version = 4, count = 1):
-	print("")
-	print("function: generatep")
-	print("function version:" + str(version))
-	print("function count:" + str(count))
+	#print("")
+	#print("function: generatep")
+	#print("function version:" + str(version))
+	#print("function count:" + str(count))
 	#print(uuid.uuid4())s
+
+	for i in range(1, count + 1):
+		print_uuid()
