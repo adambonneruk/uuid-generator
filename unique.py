807e443a914cb8ae00af8363ebf301f4ddf064b6
adambonneruk <adam@bonner.uk>
2020-09-05 13:33:11 +0100

    split help file into other module

--- unique.py
+++ unique.py
@@ -1,3 +1,14 @@
 import uuid
-outputUuidString=uuid.uuid4()
-print(outputUuidString)
+import sys
+
+from unique_help import help
+
+if len(sys.argv) == 1: # default bahviour, just generate a single UUIdv4 (no arguments)
+	outputUuidString=uuid.uuid4()
+	print(outputUuidString)
+else:
+	if sys.argv[1] == '--help':
+		help()
+	else:
+		outputUuidString=uuid.uuid4()
+		print(outputUuidString)
