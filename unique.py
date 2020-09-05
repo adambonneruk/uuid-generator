1554282c635b369805432d44ea962dedba53ea44
adambonneruk <adam@bonner.uk>
2020-09-05 15:33:15 +0100

    except handling and help done

--- unique.py
+++ unique.py
@@ -10,20 +10,31 @@ os.system("cls")
 ###############################################################################
 
 args = len(sys.argv) - 1 # number of arguments (minus the program itself)
+print("args: " + str(args))
+#set defaults
+version = 4
+count = 1
+print("v: " + str(version))
+print("c: " + str(count))
 
+try:
+	for i in range(1, args + 1): # 1 to the number of arguments
+		if re.search(r"^(-v|--version)$",sys.argv[i]):
+				version = sys.argv[i+1]
+		if re.search(r"^(-c|--count)$",sys.argv[i]):
+				count = sys.argv[i+1]
 
+	print("V: " + str(version))
+	print("C: " + str(count))
+	print("------------------------------")
 
-if args == 0:
-	generate()
-elif args == 2:
-	print("...two")
-	if re.search(r"^(-v|--version)$",sys.argv[1]):
-		generate(sys.argv[2],1)
-	elif re.search(r"^(-c|--count)$",sys.argv[1]):
-		generate(4,sys.argv[2])
+	if args == 0:
+		generate()
+	elif args == 2:
+		print("generate")
+	elif args == 4:
+		print("generate")
 	else:
 		help()
-elif args == 4:
-	print("...four")
-else:
+except:
 	help()
