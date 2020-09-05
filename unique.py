6d9ae3feea0a524342ea49a6c1281915bba36d3e
adambonneruk <adam@bonner.uk>
2020-09-05 12:50:34 +0100

    down to 3 lines

--- unique.py
+++ unique.py
@@ -1,12 +1,8 @@
 import uuid
-import sys
-import os
-
-print("Python %s\n" % (sys.version,))
-
 outputUuidString=uuid.uuid4()
 print(outputUuidString)
 '''
+print("Python %s\n" % (sys.version,))
 for x in range (1,21): #21 does 20 times, seems normal python thing?
 
 	unique1=uuid.uuid4()
@@ -14,13 +10,12 @@ for x in range (1,21): #21 does 20 times, seems normal python thing?
 	#print("UUID %d" % (x)) #too complex
 	print ('UUID [' + str(x).zfill(2) + ']: ' + str(unique1)) #simple string concatenate
 	#                 1      2     3            4   5
-	
+
 		1 = str function converts decimal x to string
 		2 = zfill pads the string to 2 chars
 		3 = sets number of pad zeros
 		4 = str function converts uuid unique1 to string
 		5 = the uuid type var called unique1
-	
-'''
-os.system("echo.")	
+os.system("echo.")
 os.system("pause")
+'''
