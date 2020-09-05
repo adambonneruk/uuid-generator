457ff38f16bd4e87d78425d989e11ac094d720d1
adambonneruk <adam@bonner.uk>
2020-09-05 14:40:34 +0100

    simplified main code, removed comments and ideas

--- unique.py
+++ unique.py
@@ -11,47 +11,13 @@ os.system("cls")
 args = len(sys.argv) - 1 # number of arguments (minus the program itself)
 
 if args == 0: #no arguments, just run with default single UUIDv4
+	print("...zero")
 	generate()
-elif args == 2:
+elif args == 2: #just one argument and parameter pair (number or version)
 	print("...two")
-elif args == 4:
+	generate()
+elif args == 4: #both version and number specified
 	print("...four")
+	generate()
 else: #any other (wrong) number of arguments, display help
 	help()
-'''
-no arguments
-	# nothing, just run with defaults
-	print("zero")
-one or less arguments
-	# not enough for anything valid
-	help()
-	print("zero or one")
-two argumnets
-	# only valid scenario is help
-	help()
-	print("two")
-three argumnets
-	# could be just -v X or just -n X
-	# unqiue -v 2
-	print("three")
-four arguments
-	# invalid as missing a value for one flag
-	help()
-	print("four")
-five arguments
-	print("five")
-six or more
-	help
-'''
-
-''' DUMB CODE
-if len(sys.argv) == 1: # default bahviour, just generate a single UUIdv4 (no arguments)
-	outputUuidString=uuid.uuid4()
-	print(outputUuidString)
-else:
-	if sys.argv[1] == '--help':
-		help()
-	else:
-		outputUuidString=uuid.uuid4()
-		print(outputUuidString)
-'''
