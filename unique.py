d38e8b581c9a3e11f65af1fe426a5fd61a80c388
adambonneruk <adam@bonner.uk>
2020-09-05 12:49:27 +0100

    removed loop/bloat

--- unique.py
+++ unique.py
@@ -4,6 +4,9 @@ import os
 
 print("Python %s\n" % (sys.version,))
 
+outputUuidString=uuid.uuid4()
+print(outputUuidString)
+'''
 for x in range (1,21): #21 does 20 times, seems normal python thing?
 
 	unique1=uuid.uuid4()
@@ -11,13 +14,13 @@ for x in range (1,21): #21 does 20 times, seems normal python thing?
 	#print("UUID %d" % (x)) #too complex
 	print ('UUID [' + str(x).zfill(2) + ']: ' + str(unique1)) #simple string concatenate
 	#                 1      2     3            4   5
-	'''
+	
 		1 = str function converts decimal x to string
 		2 = zfill pads the string to 2 chars
 		3 = sets number of pad zeros
 		4 = str function converts uuid unique1 to string
 		5 = the uuid type var called unique1
-	'''
-
+	
+'''
 os.system("echo.")	
 os.system("pause")
