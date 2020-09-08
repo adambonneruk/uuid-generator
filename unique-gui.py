816d508c68bc941bcaea29996d759abe5903ea94
adambonneruk <adam@bonner.uk>
2020-09-08 14:11:35 +0100

    minsize, title and icon

--- unique-gui.py
+++ unique-gui.py
@@ -5,4 +5,7 @@ root = Tk()
 w = Label(root, text="Hello, world!")
 w.pack()
 
+root.title('Unique: The UUID Generator') #https://stackoverflow.com/questions/33637292/change-tkinter-frame-title
+root.minsize(800,600)
+root.iconbitmap('./icon/icon.ico') #https://www.delftstack.com/howto/python-tkinter/how-to-set-window-icon-in-tkinter/
 root.mainloop()
