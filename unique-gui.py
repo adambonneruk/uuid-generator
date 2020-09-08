8133a9375cb7c5b2ee2c22d3d6eea6ad97bc21fa
adambonneruk <adam@bonner.uk>
2020-09-09 00:21:48 +0100

    working on gui, pulling in design and utilising frames

--- unique-gui.py
+++ unique-gui.py
@@ -20,27 +20,25 @@ menuBar.add_cascade(label="Options", menu=optionsMenu)
 
 window.config(menu=menuBar)
 
-labelHello = tk.Label(window, text="Hello, world!")
-labelHello.pack()
+#labelHello = tk.Label(window, text="Hello, world!")
+#labelHello.pack()
 
-labelUsername = tk.Label(window, text="User Name:")
-labelUsername.pack(side="left")
+#canvas = tk.Canvas(window, width=800, height=600)  # define the size
+#canvas.pack()
 
-text = tk.Text(window)
-text.pack()
+photo = tk.PhotoImage(file="./design/mockup-cut.png")
+imgLabel = tk.Label(window, image=photo)
+imgLabel.pack(side="left")
 
-bigtext = tk.scrolledtext(window)
-bigtext.pack()
-
-canvas = tk.Canvas(window, width=800, height=600)  # define the size
-canvas.pack()
+for fm in ['blue','red','yellow','green','white','black']:
+	tk.Frame(height = 20,width = 640,bg = fm).pack()
 
 #label = tk.Label(frame.frame,text='General Inf', bd='3', fg='blue', font='Helvetica 9 bold')  # placing labels
 #entry_field = tk.Entry(frame.frame, bd='3', justify="center")
 #check_button = tk.Checkbutton(frame.frame_test, text="1", variable= check_1, onvalue=1, offvalue=0)
 
-frame = tk.Frame(window, bg="grey")
-frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+#frame = tk.Frame(window, bg="grey")
+#frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
 
 window.title('Unique: The UUID Generator') #https://stackoverflow.com/questions/33637292/change-tkinter-frame-title
 window.iconbitmap('./icon/icon.ico') #https://www.delftstack.com/howto/python-tkinter/how-to-set-window-icon-in-tkinter/
