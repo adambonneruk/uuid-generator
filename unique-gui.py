751ff5b539527b4f50f22d98efc21bf60deb4fc7
adambonneruk <adam@bonner.uk>
2020-09-13 16:10:40 +0100

    add v4 generation

--- unique-gui.py
+++ unique-gui.py
@@ -1,11 +1,33 @@
 import tkinter as tk
+from tkinter import simpledialog
+import logging
+
 from toast import quit, about
+from generate_uuid import generate_uuid
+
+debug = True
+if debug:
+	logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+	logging.debug("DEBUG MODE ACTIVE")
+
+count = 0
 
 # Create the Window
 window = tk.Tk()
-window.title("Unique: The UUID Generator")
+window.title("Unique: UUID Generator")
 window.iconbitmap("./icon/icon.ico")
-window.geometry("512x256")
+window.geometry("310x275")
+
+def uuid4():
+	global count
+	if count == 0:
+		quantity()
+	for i in range(0,count):
+		plainText = plainTextArea.get('1.0', "end"+'-1c')
+		uuid = generate_uuid()
+		if plainText != "":
+			plainTextArea.insert("end","\n")
+		plainTextArea.insert("end",uuid)
 
 #Create Text Area
 plainTextArea = tk.Text(window)
@@ -25,13 +47,20 @@ fileMenu.add_separator()
 fileMenu.add_command(label="Exit",command=lambda: quit(window))
 menuBar.add_cascade(label="File", menu=fileMenu)
 
+def quantity():
+	global count
+	count = simpledialog.askinteger("UUID Quantity", "Configure Number of UUIDs to Generate")
+	return count
+
 uuidMenu = tk.Menu(menuBar,tearoff=0)
-uuidMenu.add_command(label="Version 0")
-uuidMenu.add_command(label="Version 1")
-uuidMenu.add_command(label="Version 4")
+#uuidMenu.add_command(label="Version 0")
+#uuidMenu.add_command(label="Version 1")
+uuidMenu.add_command(label="Version 4",command=uuid4)
+#uuidMenu.add_separator()
+#uuidMenu.add_command(label="Version 3")
+#uuidMenu.add_command(label="Version 5")
 uuidMenu.add_separator()
-uuidMenu.add_command(label="Version 3")
-uuidMenu.add_command(label="Version 5")
+uuidMenu.add_command(label="Quantity",command=lambda: quantity())
 menuBar.add_cascade(label="Generate UUIDs", menu=uuidMenu)
 
 helpMenu = tk.Menu(menuBar,tearoff=0)
