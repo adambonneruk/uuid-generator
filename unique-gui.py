66d7cef5b1ec3b31db5ac5d6336d0d57c0f92622
adambonneruk <adam@bonner.uk>
2020-09-13 13:51:50 +0100

    added gui.py, including window

---
+++ unique-gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Create the Window
+window = tk.Tk()
+window.title('Unique: The UUID Generator')
+window.iconbitmap('./icon/icon.ico')
+window.geometry("512x256")
+
+# Start the Window Main Loop
+window.mainloop()
