3cffb5a11830782b664729f62a462ec7a338770f
adambonneruk <adam@bonner.uk>
2020-09-18 01:15:51 +0100

    removed unneeded files new gui filename

--- build_python.bat
+++ build_python.bat
@@ -1,2 +1,2 @@
 pyinstaller.exe --onefile --icon=./icon/icon.ico unique.py
-pyinstaller.exe --onefile --icon=./icon/icon.ico unique-gui.pyw
+pyinstaller.exe --onefile --icon=./icon/icon.ico unique_gui.py
