6f80c68e8c0aafa8f05d1ea39e19273038b25039
adambonneruk <adam@bonner.uk>
2020-09-20 23:49:55 +0100

    elongate as opposite of shorten function

--- short.py
+++ short.py
@@ -24,6 +24,26 @@ def shorten_uuid(input_uuid):
 
     return output_uuid
 
+def elongate_uuid(input_uuid):
+    """"convert base64 to hexadecimal uuid"""
+    logging.debug("elongate_uuid():")
+
+    b64_uuid = input_uuid.encode("ascii")
+    logging.debug("b64_uuid: %s", str(b64_uuid))
+
+    bin_uuid = codecs.decode(b64_uuid, 'base64')
+    logging.debug("bin_uuid: %s", str(bin_uuid))
+
+    hex_uuid = codecs.encode(bin_uuid, 'hex')
+    logging.debug("hex_uuid: %s", str(hex_uuid))
+
+    str_uuid = uuid.UUID(str(hex_uuid)[2:-1].zfill(32))
+    logging.debug("str_uuid: %s", str(str_uuid))
+
+    output_uuid = str(str_uuid)
+
+    return output_uuid
+
 def main():
     """using main for testing purposes"""
     debug = True
@@ -37,6 +57,14 @@ def main():
 
     encoded_uuid = shorten_uuid(test_uuid)
     logging.debug("base64 encoded uuid: %s", encoded_uuid)
+
+    print("===========================")
+
+    decoded_uuid = elongate_uuid(encoded_uuid)
+    logging.debug("...and back again: %s", decoded_uuid)
+
+    little_uuid = elongate_uuid("QACgAAAAAAAL/w==")
+    logging.debug("...and back again: %s", little_uuid)
 
 if __name__ == "__main__":
     main()
